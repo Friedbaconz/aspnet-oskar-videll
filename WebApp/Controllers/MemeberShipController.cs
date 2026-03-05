9a06c901030f4cdcc7ad2cb852df655bc8d50e4d
Oskar <Oskarwidell@outlook.com>
2026-03-05 13:51:12 +0100

    First push: Just basic structuring of controllers and Views. With a small start on Auth(signup).

---
+++ WebApp/Controllers/MemeberShipController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class MemeberShipController : Controller
+    {
+        public IActionResult NewMembership()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewMembership(string username)
+        {
+            return View();
+        }
+
+        public IActionResult ConnectMembership()
+        {
+            return View();
+        }
+
+        public IActionResult MembershipStatus()
+        {
+            return View();
+        }
+    }
+}
