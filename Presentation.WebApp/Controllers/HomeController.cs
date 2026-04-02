2d492b07267ca770d320219e69277b3a5fa0fd1a
Oskar <Oskarwidell@outlook.com>
2026-04-02 16:42:44 +0200

    added a error section viewmodel

--- Presentation.WebApp/Controllers/HomeController.cs
+++ Presentation.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Models.Error;
+using System.Diagnostics;
 
 namespace Presentation.WebApp.Controllers;
 
@@ -10,5 +12,11 @@ public class HomeController : Controller
         ViewData["Title"] = "Home";
 
         return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ Presentation.WebApp/Models/Error/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Presentation.WebApp.Models.Error;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool? Success => !string.IsNullOrEmpty(RequestId);
+}
